17e35d22d787f01ff65af99c86fcaed3c10f6e12
Ben Maharjan <benmaharjan@hotmail.co.uk>
2024-09-11 15:57:09 +0100

    API frontend

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,25 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [fishSearch, setFishSearch] = useState("")
+  const [seaLevel, setSeaLevel] = useState("")
+  const [dateTime, setDateTime] = useState("")
+  const [weatherMain, setWeatherMain] = useState("")
+  const [weatherDesc, setWeatherDesc] = useState("")
 
-  function handleFishChange(event) {
+  function handleFishChange(event:any) {
     setFishSearch(event.target.value);
   }
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async (event:any) => {
     event.preventDefault();
-    console.log(fishSearch);
+    //console.log(fishSearch);
     let cityData = await getCity(fishSearch);
-    console.log(cityData);
+    //console.log(cityData);
     let weatherData = await getWeather(cityData[0].lat, cityData[0].lon);
-    console.log(weatherData);
+    setSeaLevel(weatherData.list[0].main.sea_level);
   }
 
-  async function getCity(query) {
+  async function getCity(query:string) {
     try {
       const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=70bba6d533205abf96963d03760d52e6`, {
       });
@@ -33,7 +37,7 @@ function App() {
     }
   }
 
-  async function getWeather(lat, lon) {
+  async function getWeather(lat:number, lon:number) {
     try {
       const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=70bba6d533205abf96963d03760d52e6`, {
       });
@@ -61,10 +65,10 @@ function App() {
           <button type="submit">Avoid</button>
         </form>
         <section id="content">
-          <p>dt_txt</p>
-          <p>weather.main</p>
-          <p>weather.description</p>
-          <p>main.sea_level</p>
+          <p>dt_txt {seaLevel}</p>
+          <p>weather.main {seaLevel}</p>
+          <p>weather.description {seaLevel}</p>
+          <p>Main sea level: {seaLevel}</p>
         </section>
       </main>
       <footer>
